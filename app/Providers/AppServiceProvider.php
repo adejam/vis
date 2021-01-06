59cb92bf713943a1311cdf5dc0397b69551d18dc
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-01-06 15:32:10 +0100

    chore: update defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        schema::defaultStringLength(255);
+        schema::defaultStringLength(191);
     }
 
     /**

15f9bc78dae4c6ffba8ecb4c18cf277018504824
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-01-16 02:02:29 +0100

    fix: fix merge issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-<<<<<<< HEAD
-// use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\CanResetPassword;
-=======
->>>>>>> development
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;

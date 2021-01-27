f93c1f0fb8111488c7dd1e82e10e14ac63f52e26
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-01-28 00:17:37 +0100

    feat:remove comments

--- resources/views/user/my-community/allVehicleUsers.blade.php
+++ resources/views/user/my-community/allVehicleUsers.blade.php
@@ -4,8 +4,7 @@
         <section class="p-3 mb-3 mx-3 mt-10 text-center border-gray-100 border hover:bg-lightblue rounded-full">
             <a class=" font-semibold text-primary w-full flex justify-between"
                 href="{{ url('my-community/' . $communityId . '/vehicle-users') }}">
-                {{ __('Verified Vehicle Users') }} 
-                {{-- <i class="inline-block py-1 px-2 text-xs font-semibold text-center whitespace-no-wrap align-baseline rounded-full bg-green-400 text-white">{{ $communityVehicleUsers }}</i> --}}
+                {{ __('Verified Vehicle Users') }}
             </a>
         </section>
 
@@ -13,7 +12,6 @@
             <a class=" font-semibold text-primary w-full flex justify-between"
                 href="{{ url('my-community/' . $communityId . '/community-vehicle-users') }}">
                 {{ __('Added Vehicle Users') }} 
-                {{-- <i class="inline-block py-1 px-2 text-xs font-semibold text-center whitespace-no-wrap align-baseline rounded-full bg-green-400 text-white">{{ $communityVehicleUsers }}</i> --}}
             </a>
         </section>
 </x-app-layout>
